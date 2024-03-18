d209a744d78a3d039d38aa4af100ceb96ef54ffb
Jelle Zwiers <public@jellezwie.rs>
2024-03-18 10:50:46 +0100

    Make map work

---
+++ app/commune-map.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import { useState } from 'react';
+
+import 'mapbox-gl/dist/mapbox-gl.css'; // Mapbox
+// import 'maplibre-gl/dist/maplibre-gl.css'; // Maplibre
+import Map, { GeolocateControl, NavigationControl, FullscreenControl, ScaleControl, Source, Layer, Marker, Popup, MapRef } from 'react-map-gl';
+// import Map, { GeolocateControl, NavigationControl, FullscreenControl, ScaleControl, Source, Layer, Marker, Popup } from 'react-map-gl/maplibre'; // Maplibre
+
+import './commune-map.css';
+
+export default function CommuneMap({ data }: { data: any }) {
+  const [popup, setPopup] = useState(null);
+
+  return (
+    <Map
+      initialViewState={{
+        longitude: 10.44783,
+        latitude: 51.16382,
+        zoom: 5
+      }}
+      style={{ position: 'absolute', width: '100%', height: '100%' }}
+
+      cooperativeGestures={false}
+      attributionControl={false}
+
+      mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN} // Mapbox
+      mapStyle="mapbox://styles/jelle619/clsewc2fo01rg01pedk0774k7" // Mapbox
+    // mapStyle={"https://demotiles.maplibre.org/style.json"} // MapLibre
+    >
+      <NavigationControl position="bottom-left" />
+
+      {data.map((entry: any, index: any) => (
+        <>
+          <Marker key={entry.name} longitude={Number(entry.coordinates.split(", ")[1])} latitude={Number(entry.coordinates.split(", ")[0])} anchor="bottom" offset={[0, 6]}
+            onClick={e => {
+              // If we let the click event propagates to the map, it will immediately close the popup
+              // with `closeOnClick: true`
+              e.originalEvent.stopPropagation();
+              setPopup(entry.name);
+            }}
+          />
+
+          {popup == entry.name && (
+            <Popup
+              key={entry.name}
+              anchor="top"
+              longitude={Number(entry.coordinates.split(", ")[1])}
+              latitude={Number(entry.coordinates.split(", ")[0])}
+              onClose={() => setPopup(null)}
+            >
+              <div className="text-white">
+                <h1>{entry.name}</h1>
+              </div>
+            </Popup>
+          )}
+        </>
+      ))}
+
+    </Map>
+
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import * as xlsx from 'xlsx';
+import CommuneMap from './commune-map';
 
 // load 'fs' for readFile and writeFile support
 import * as fs from 'fs';
@@ -13,19 +14,30 @@ import * as cpexcel from 'xlsx/dist/cpexcel.full.mjs';
 xlsx.set_cptable(cpexcel);
 
 async function fetchData() {
-  const url = "https://sheetjs.com/data/PortfolioSummary.xls"
-  const file = await (await fetch(url, {redirect: "follow"})).arrayBuffer();
+  const url = "https://www.googleapis.com/drive/v3/files/1fPM_4ubBrFDJudcbh4nWyu7WnV5YSk1q?alt=media&key=" + process.env.GOOGLE_DRIVE_API_KEY
+  const file = await (await fetch(url, {redirect: "follow", next: { revalidate: 600 }})).arrayBuffer();
   const workbook = xlsx.read(file);
   const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-  console.log(worksheet);
+  const data = xlsx.utils.sheet_to_json(worksheet, {
+    blankrows: false
+  });
+
+  const filteredData = data.filter((entry: any) =>
+  entry.coordinates !== undefined &&
+  entry.coordinates !== null &&
+  !isNaN(entry.coordinates.split(", ")[0]) &&
+  !isNaN(entry.coordinates.split(", ")[1]) &&
+  Object.keys(entry.coordinates).length !== 0);
+
+  return filteredData;
 }
 
 export default async function Home() {
-  await fetchData();
+  const data = await fetchData();
+
   return (
     <>
-      <h1>Cahcaw</h1>
-      <p>This is a placeholder website.</p>
+      <CommuneMap data={data}/>
     </>
   )
 }
